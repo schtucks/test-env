e2c1f9112daa03425f854f8d5f18f0c2efde52bc
fehi <ferdinand.hirz@zentro.at>
2024-05-21 09:54:05 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import Quill, { Delta, RangeStatic } from 'quill';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,92 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-env';
+  @Input()
+  message: string;
+
+  quill: Quill;
+
+  getEditorInstance(event: Quill): void {
+    this.quill = event;
+    const toolbar: any = this.quill.getModule('toolbar');
+    const clipboard: any = this.quill.getModule('clipboard');
+    toolbar.addHandler('image', this.customImageHandler);
+    clipboard.addMatcher('*', this.customImagePasteHandler);
+  }
+
+  getMessage(): string {
+    return this.message;
+  }
+
+  contentChanged(obj: any): void {
+    this.message = obj.html;
+  }
+
+  /**
+   * Handles custom image insertion into the Quill editor.
+   * 
+   * This method is defined as an arrow function to ensure lexical binding of 'this'.
+   * When used as a callback, arrow functions maintain the context of the surrounding class,
+   * allowing access to class properties and methods via 'this'.
+   * 
+   * @param {boolean} openExplorer indicating whether to open the file explorer to select an image.
+   * @returns void
+   */
+  customImageHandler = (openExplorer: boolean) => {
+    if (openExplorer) {
+      this.handleFileExplorer().then((result: string) => {
+        this.insertImageAtRange(result);
+      }).catch((error: Error) => {
+        console.error(error);
+      });
+    }
+  }
+  
+  insertImageAtRange(base64String) {
+    const range: RangeStatic = this.quill.getSelection();
+    this.quill.insertEmbed(range.index, 'image', base64String, 'user');
+  }
+
+  handleFileExplorer(): Promise<string> {
+    return new Promise((resolve, reject) => {
+      try {
+        const fileInput: HTMLInputElement = document.createElement("input");
+        fileInput.type = "file";
+        fileInput.accept = "image/png, image/jpeg";
+        fileInput.onchange = (event) => {
+          const file: File | null = (event.target as HTMLInputElement).files?.[0] || null;
+          if (!file) {
+            reject(new Error("No file selected."));
+            return;
+          }
+
+          const reader: FileReader = new FileReader();
+          reader.onload = () => {
+            const base64String: string | null = reader.result as string;
+            if (base64String) {
+              resolve(base64String);
+            } else {
+              reject(new Error("Failed to read the file."));
+            }
+          };
+          reader.onerror = () => {
+            reject(new Error("Error reading the file."));
+          };
+          reader.readAsDataURL(file);
+        };
+
+        fileInput.click();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  customImagePasteHandler(node: HTMLElement, delta: Delta) {
+    if (node && delta) {
+      console.log(node);
+      console.log(delta);
+    }
+    return delta;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatLegacyTooltipModule as MatTooltipModule } from '@angular/material/legacy-tooltip';
+import { QuillModule } from 'ngx-quill'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTooltipModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
